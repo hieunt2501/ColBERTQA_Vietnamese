edea4074fdccbe2bf055eecc8e793b97670b92a2
nthieu <nthieu2501@gmail.com>
2022-04-12 08:56:28 +0000

    fix bug load ref

--- colbert/evaluation/slow.py
+++ colbert/evaluation/slow.py
@@ -19,8 +19,8 @@ def slow_rerank(args, query, pids, passages, sent_ref=""):
 
     if sent_ref:
         # sent ref: dict of key sentence id and value is tuple of passage id and passage
-        ranked_pids = [sent_ref[sid][0] for sid in ranked_pids]
         ranked_passages = [sent_ref[sid][1] for sid in ranked_pids]
+        ranked_pids = [sent_ref[sid][0] for sid in ranked_pids]
     else:
         ranked_passages = [passages[position] for position in ranked]  
 
